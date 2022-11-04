8377203d1013618c1bc12b6e299773daaf4396d1
Andy Pearson <andy@londonparkour.com>
2022-11-04 08:33:14 +0000

    Update icon strokes

--- app/client/highlightlist/check_highlight_list.js
+++ app/client/highlightlist/check_highlight_list.js
@@ -18,7 +18,7 @@ export function check_highlight_list(currentPropertyID)
     highlightlist.forEach(function(propertyColourArray, index) {
 
         if ( propertyColourArray.propertyID == currentPropertyID) {
-            newIcon = "https://svg-rewriter.sachinraja.workers.dev/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fnpm%2F%40mdi%2Fsvg%406.7.96%2Fsvg%2Fhome-circle.svg&fill=%23"+propertyColourArray.colour+"&width=20px&height=20px";
+            newIcon = "https://svg-rewriter.sachinraja.workers.dev/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fnpm%2F%40mdi%2Fsvg%406.7.96%2Fsvg%2Fhome-circle.svg&fill=%23"+propertyColourArray.colour+"&stroke=%23ffffff&stroke-width=0.5&width=20px&height=20px";
         }
 
     })
